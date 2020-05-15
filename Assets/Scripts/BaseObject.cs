0ed3c34f49b91612a77d91fd5b495406275fdd1a
scrible2m <scrible2m@gmail.com>
2020-05-15 15:54:53 +0300

    15.05
    15.05

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseObject : MonoBehaviour
+{
+    private Transform _GOtransform;
+    private GameObject _GOInstance;
+    private string _name;
+    private bool _isVisible;
+
+    private Vector3 _position;
+    private Vector3 _scale;
+    private Quaternion _rotation;
+
+    private Rigidbody _rigidbody;
+    private Material _material;
+    private Color _color;
+
+    private Animator _animator;
+
+    protected Transform GOtransform { get => _GOtransform; set => _GOtransform = value; }
+    protected GameObject GOInstance { get => _GOInstance;}
+    protected string Name 
+    { get => _name;
+        set
+        {
+            _name = value;
+            gameObject.name = _name;
+        }
+    }
+    protected bool IsVisible 
+    { get => _isVisible;
+        set
+        {
+            _isVisible = value;
+            if (GetComponent<Renderer>())
+            {
+                GetComponent<Renderer>().enabled = _isVisible;
+            }
+        }
+    }
+    protected Vector3 Position { get => _position; set => _position = value; }
+    protected Vector3 Scale 
+    { get
+        {
+            _scale = _GOtransform.localScale;
+            return _scale;
+        }
+
+        set
+        {
+            _scale = value;
+            _GOtransform.localScale = _scale;
+        }
+    }
+    protected Quaternion Rotation { get => _rotation; set => _rotation = value; }
+    protected Rigidbody Rogodbody { get => _rigidbody;}
+    protected Material Material { get => _material;}
+    protected Color Color { get => _color; set => _color = value; }
+    protected Animator Animator { get => _animator;}
+
+   
+    protected virtual void Awake()
+    {
+        _GOInstance = gameObject;
+        _GOtransform = gameObject.transform;
+        _name = gameObject.name;
+
+        if (GetComponent<Rigidbody>())
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        if (GetComponent<Animator>())
+        {
+            _animator = GetComponent<Animator>();
+        }
+        if (GetComponent<Renderer>())
+        {
+           _material = GetComponent<Renderer>().material;
+        
+        _color = GetComponent<Renderer>().material.color;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Flashlight : BaseObject
+{
+    private float _lighttime = 10;
+    private float _timer;
+    private Light _light;
+    private KeyCode clic = KeyCode.F;
+    private bool _isLighting;
+    public GameObject[] batt;
+    public GameObject lightSprite;
+    public GameObject redBat;
+    private int batCount = 5;
+    void Start()
+    {
+        _light = GetComponentInChildren<Light>();
+        _timer = _lighttime;
+    }
+
+    private void FlashLightClic(bool val)
+    {
+        _light.enabled = val;
+        lightSprite.SetActive(val);
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        int i = Mathf.RoundToInt(_timer / 2);
+        if (i<1)
+        {
+            redBat.SetActive(true);
+        }
+        else
+        {
+            redBat.SetActive(false);
+        }
+        if (i< batCount && i>0)
+        {
+            batt[batCount-1].SetActive(false);
+            batCount = i;
+        }
+        else if (i>batCount)
+        {
+            batt[batCount].SetActive(true);
+            if (i > 0)
+            {
+                batCount = i;
+            }
+            else if (i<0)
+            {
+                batCount = 0;
+            }
+        }
+
+       
+
+        if (Input.GetKeyDown(clic))
+        {
+            _isLighting = !_isLighting;
+            FlashLightClic(_isLighting);
+        }
+        if (_isLighting)
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <0)
+            {
+                FlashLightClic(false);
+                _isLighting = false;
+            }
+        }
+        else if (!_isLighting && _timer<_lighttime)
+        {
+            _timer += Time.deltaTime / 2;
+        }
+    }
+}
