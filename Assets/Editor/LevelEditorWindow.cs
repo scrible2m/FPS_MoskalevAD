3e97c9f9e0994ee434f618cfeec1899c91bb3ebe
scrible2m <51215544+scrible2m@users.noreply.github.com>
2020-05-29 18:47:18 +0300

    PrefabEditor

--- Assets/Editor/LevelEditorWindow.cs
+++ Assets/Editor/LevelEditorWindow.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using Unity.IO;
+using System.IO;
+using UnityEngine.UI;
 
 public class LevelEditorWindow : EditorWindow
 {
+
+    List<Folders> _folders = new List<Folders>();
+    bool _reset = true;
     
-    public List<Object> _sciFi = new List<Object>();
-    
+    int j = 0;
+
 
     [MenuItem("MyTools/LevelEditor")]
     public static void ShowWindow()
@@ -16,31 +20,107 @@ public class LevelEditorWindow : EditorWindow
         GetWindow(typeof(LevelEditorWindow), false, "LevelEditor");
     }
 
+
+
     private void OnGUI()
     {
 
 
 
-        
 
-        string[] temp = AssetDatabase.GetSubFolders("Assets/LevelEditorPrefabs");
-        foreach (string _folder in temp)
+        if (_reset)
         {
-            EditorGUILayout.LabelField(_folder.Substring(26) + " Prefabs", EditorStyles.boldLabel);
-            Object[] _prefBase = AssetDatabase.LoadAllAssetsAtPath(_folder);
-            EditorGUILayout.LabelField(_folder + " Prefabs", EditorStyles.boldLabel);
             
-            foreach(Object _pref in _prefBase)
-            {
-                Debug.Log("!!!");
+            _folders.Clear();
+            string[] tempReset = AssetDatabase.GetSubFolders("Assets/LevelEditorPrefabs");
+
+
+
+
+
+            foreach (string _folder in tempReset)
+
+            { 
+
+
+                DirectoryInfo dirInfo = new DirectoryInfo(_folder);
+                FileInfo[] fileInf = dirInfo.GetFiles("*.prefab");
+               
+                
+                foreach (FileInfo fileInfo in fileInf)
+                {
+                    
+                    string fullPath = fileInfo.FullName.Replace(@"\", "/");
+                    string assetPath = "Assets" + fullPath.Replace(Application.dataPath, "");
+                    GameObject prefab = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
+
+                    if (prefab != null)
+                    {
+                        _folders.Add(new Folders() { _GO = prefab, Name = _folder.Substring(26), Count = fileInf.Length }) ;
+
+                    }
+
+                   
+                }
+               
             }
-
-
-
-
-
         }
 
+
+
+        j = 0;
+        foreach (Folders _goList in _folders)
+
+        {
+            if (j==0)
+            {
+                EditorGUILayout.LabelField(_goList.Name + " Prefabs", EditorStyles.boldLabel);
+                j = 0;
+            }
+           
+           if (j!=_goList.Count)
+            {
+                
+                EditorGUILayout.LabelField(_goList.Name + " Prefabs" , EditorStyles.boldLabel);
+                j = 0;
+                
+            }
+            j = _goList.Count;
+            
+
+            if (_goList._GO != null)
+            {
+                
+                   
+                        if (GUILayout.Button(_goList._GO.name))
+                        {
+
+                        }
+                
+            }
+
+            
+        }
+
+
+
+        _reset = false;
+
+        EditorGUILayout.LabelField("_RESET", EditorStyles.boldLabel);
+        if (GUILayout.Button("Reset"))
+        {
+            _reset = true;
+            j = 0;
+        }
+
+        
+    }
+
+    public class Folders
+    {
+        public GameObject _GO;
+        public string Name;
+        public int Count;
         
     }
    
